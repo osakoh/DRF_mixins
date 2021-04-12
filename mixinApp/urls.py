6c4cc8e7895a0febc46b4ef29172533ff303ec75
osakoh <macmive@gmail.com>
2021-04-12 23:22:22 +0100

    configured defaultrouter

--- mixinApp/urls.py
+++ mixinApp/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
+router = DefaultRouter()
+router.register('student', views.StudentViewSet)
+
 urlpatterns = [
-    path('', views.StudentList.as_view()),
-    path('<int:pk>/', views.StudentDetail.as_view()),
+    # path('', views.StudentList.as_view()),
+    # path('<int:pk>/', views.StudentDetail.as_view()),
 ]
